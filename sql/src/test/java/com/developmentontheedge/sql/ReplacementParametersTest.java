4cf8164d5687f9af32424d89f4ff1286044aacd6
QProgS <uu.innk@gmail.com>
2017-03-26 04:51:19 +0700

    add ReplacementParameter ?

---
+++ sql/src/test/java/com/developmentontheedge/sql/ReplacementParametersTest.java
@@ -0,0 +1,39 @@
+package com.developmentontheedge.sql;
+
+import com.developmentontheedge.sql.model.SqlQuery;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReplacementParametersTest
+{
+
+    @Test
+    public void testSimple()
+    {
+        String query = "SELECT id FROM persons WHERE name = ?";
+        assertEquals(query, SqlQuery.parse( query ).format());
+    }
+
+    @Test
+    public void test2()
+    {
+        String query = "SELECT id FROM persons WHERE name = ? AND title = ?";
+        assertEquals(query, SqlQuery.parse( query ).format());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testErrorInSelectList()
+    {
+        String query = "SELECT ? FROM persons";
+        assertEquals(query, SqlQuery.parse( query ).format());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReplacementParametersErrorInFrom()
+    {
+        String query = "SELECT name FROM ?";
+        assertEquals(query, SqlQuery.parse( query ).format());
+    }
+
+}
