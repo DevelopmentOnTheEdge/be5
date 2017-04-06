46b11f3888963af85d1c0b889a1dcfc524e425d4
uuinnk <uuinnk@developmentontheedge.com>
2017-04-06 16:40:04 +0700

    multiple category support for actions

--- server/src/main/java/com/developmentontheedge/be5/components/ScriptList.java
+++ server/src/main/java/com/developmentontheedge/be5/components/ScriptList.java
@@ -18,30 +18,45 @@ import com.developmentontheedge.be5.api.exceptions.impl.Be5ErrorCode;
 
 public class ScriptList implements Component
 {
-    private static final List<String> allowedCategories = Arrays.asList("components", "actions");
+    private static final String be5Category = "be5/scripts/be5/actions";
 
     @Override
     public void generate(Request req, Response res, ServiceProvider serviceProvider)
     {
-        List<String> result = new ArrayList<>();
+        List<ActionPaths> result = new ArrayList<>();
         String scriptCategory = req.get("category");
 
-        if (!allowedCategories.contains(scriptCategory))
-            throw Be5ErrorCode.PARAMETER_INVALID.exception("category", scriptCategory);
+        //TODO find and load also modules action
+        load(req, result, be5Category);
+        load(req, result, scriptCategory);
 
-        try (Stream<Path> paths = Files.list(Paths.get(req.getRawRequest().getSession().getServletContext().getRealPath("/scripts/" + scriptCategory))))
+        res.sendAsRawJson(result);
+    }
+
+    private void load(Request req, List<ActionPaths> result, String scriptCategory)
+    {
+        try (Stream<Path> paths = Files.list(Paths.get(req.getRawRequest().getSession().getServletContext().getRealPath(scriptCategory))))
         {
             paths.map(p -> p.getFileName().toString())
                     .filter(n -> n.endsWith(".js"))
-                    .map(n -> n.substring(0, n.length() - ".js".length()))
+                    .map(n -> new ActionPaths(n.substring(0, n.length() - ".js".length()), scriptCategory + "/" + n))
                     .forEach(result::add);
         }
         catch (IOException e)
         {
             throw Be5Exception.internal(e);
         }
-        
-        res.sendAsRawJson(result);
+    }
+
+    class ActionPaths{
+        String name;
+        String path;
+
+        public ActionPaths(String name, String path)
+        {
+            this.name = name;
+            this.path = path;
+        }
     }
 
 }
