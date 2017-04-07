09895e6fabaea5abcca670a858369298207f2e32
uuinnk <uuinnk@developmentontheedge.com>
2017-04-07 13:17:15 +0700

    remove unnecessary modifiers

--- metadata/src/main/java/com/developmentontheedge/be5/metadata/DatabaseConstants.java
+++ metadata/src/main/java/com/developmentontheedge/be5/metadata/DatabaseConstants.java
@@ -7,37 +7,37 @@ public interface DatabaseConstants
 {
     // constants from RecordEx,
     // used in com.developmentontheedge.be5.components.impl.model.PropertiesToRowTransformer
-    public static final String HIDDEN_COLUMN_PREFIX = "___";
-    public static final String GLUE_COLUMN_PREFIX = "+";
-    public static final String EXTRA_HEADER_COLUMN_PREFIX = ";";
+    String HIDDEN_COLUMN_PREFIX = "___";
+    String GLUE_COLUMN_PREFIX = "+";
+    String EXTRA_HEADER_COLUMN_PREFIX = ";";
 
 
-    static final String ALL_RECORDS_VIEW = "All records";
-    static final String SELECTION_VIEW = "*** Selection view ***";
+    String ALL_RECORDS_VIEW = "All records";
+    String SELECTION_VIEW = "*** Selection view ***";
 
     // used in com.developmentontheedge.be5.components.impl.model.TableModel
-    static final String COL_ATTR_AGGREGATE = "aggregate";
-    static final String COL_ATTR_ROLES  = "roles";
-    static final String COL_ATTR_BLANKNULLS = "blankNulls";
+    String COL_ATTR_AGGREGATE = "aggregate";
+    String COL_ATTR_ROLES  = "roles";
+    String COL_ATTR_BLANKNULLS = "blankNulls";
 
-    static final String CSS_ROW_CLASS = HIDDEN_COLUMN_PREFIX + "css_class";
+    String CSS_ROW_CLASS = HIDDEN_COLUMN_PREFIX + "css_class";
 
     // used in com.developmentontheedge.be5.components.impl.model.PropertiesToRowTransformer
-    static final String ID_COLUMN_LABEL = HIDDEN_COLUMN_PREFIX + "ID";
+    String ID_COLUMN_LABEL = HIDDEN_COLUMN_PREFIX + "ID";
 
     //TODO create enum COL_ATTR
-    static final String COL_ATTR_NULLIF  = "nullif";
-    static final String COL_ATTR_LINK = "link";
+    String COL_ATTR_NULLIF  = "nullif";
+    String COL_ATTR_LINK = "link";
 
-    static final String L10N_TOPIC_DISPLAY_NAME = "displayName";
-    static final String L10N_TOPIC_VIEW_NAME = "viewName";
-    static final String L10N_TOPIC_VIEW_MENU_NAME = "viewMenuName";
-    static final String L10N_TOPIC_VIEW_TITLE_NAME = "viewTitleName";
-    static final String L10N_TOPIC_OPERATION_NAME = "operationName";
-    static final String L10N_TOPIC_VIEW_SHORT_DESCRIPTION = "viewShortDescription";
-    static final String L10N_TOPIC_PAGE = "page";
-    static final String L10N_TOPIC_CODE = "code";
-    static final String L10N_TOPIC_SCHEME = "scheme";
-    static final String L10N_TOPIC_ATTRIBUTE = "attribute";
-    static final String L10N_TOPIC_INSERT = "Insert";
+    String L10N_TOPIC_DISPLAY_NAME = "displayName";
+    String L10N_TOPIC_VIEW_NAME = "viewName";
+    String L10N_TOPIC_VIEW_MENU_NAME = "viewMenuName";
+    String L10N_TOPIC_VIEW_TITLE_NAME = "viewTitleName";
+    String L10N_TOPIC_OPERATION_NAME = "operationName";
+    String L10N_TOPIC_VIEW_SHORT_DESCRIPTION = "viewShortDescription";
+    String L10N_TOPIC_PAGE = "page";
+    String L10N_TOPIC_CODE = "code";
+    String L10N_TOPIC_SCHEME = "scheme";
+    String L10N_TOPIC_ATTRIBUTE = "attribute";
+    String L10N_TOPIC_INSERT = "Insert";
 }
