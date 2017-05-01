1635ce268d507467a931a32798ab66e2b9e73082
Innokentiy <uu.innk@gmail.com>
2017-05-01 20:58:32 +0700

    add DocumentTest

---
+++ server/src/test/java/com/developmentontheedge/be5/components/DocumentTest.java
@@ -0,0 +1,47 @@
+package com.developmentontheedge.be5.components;
+
+import com.developmentontheedge.be5.AbstractProjectTest;
+import com.developmentontheedge.be5.api.Component;
+import com.developmentontheedge.be5.api.Response;
+import com.developmentontheedge.be5.model.TablePresentation;
+import com.google.common.collect.ImmutableMap;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class DocumentTest extends AbstractProjectTest
+{
+    private static Component component;
+
+    @BeforeClass
+    public static void init(){
+        component = loadedClasses.get("document");
+    }
+
+    @Test
+    public void generate() throws Exception {
+        Response response = mock(Response.class);
+
+        component.generate(getSpyMockRequest("", ImmutableMap.of(
+                        "entity","testtable",
+                        "query","All records")), response, sp);
+
+        verify(response).sendAsJson(eq("table"), any(TablePresentation.class));
+    }
+
+    @Test
+    public void generateCount() throws Exception {
+        Response response = mock(Response.class);
+
+        component.generate(getSpyMockRequest("count", ImmutableMap.of(
+                "entity","testtable",
+                "query","All records")), response, sp);
+
+        verify(response).sendAsJson(eq(2L));
+    }
+
+}
