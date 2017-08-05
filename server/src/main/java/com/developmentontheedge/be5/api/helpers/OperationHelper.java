2875ae133a0e26ae63990dc9ba7b338a3df84e98
QProgS <uu.innk@gmail.com>
2017-08-05 16:33:56 +0700

    add tagsCache

--- server/src/main/java/com/developmentontheedge/be5/api/helpers/OperationHelper.java
+++ server/src/main/java/com/developmentontheedge/be5/api/helpers/OperationHelper.java
@@ -1,18 +1,23 @@
 package com.developmentontheedge.be5.api.helpers;
 
 import com.developmentontheedge.be5.api.exceptions.Be5Exception;
+import com.developmentontheedge.be5.api.services.CacheInfo;
 import com.developmentontheedge.be5.api.services.Meta;
 import com.developmentontheedge.be5.api.services.SqlService;
 import com.developmentontheedge.be5.metadata.DatabaseConstants;
 import com.developmentontheedge.be5.metadata.exception.ProjectElementException;
 import com.developmentontheedge.be5.metadata.model.ColumnDef;
 import com.developmentontheedge.be5.metadata.model.Query;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 import java.util.List;
 import java.util.Optional;
 
 public class OperationHelper
 {
+    private final Cache<String, String[][]> tagsCache;
+
     private final SqlService db;
     private final Meta meta;
     private final UserAwareMeta userAwareMeta;
@@ -25,8 +30,14 @@ public class OperationHelper
         this.db = db;
         this.meta = meta;
         this.userAwareMeta = userAwareMeta;
+
+        tagsCache = Caffeine.newBuilder()
+                .maximumSize(10_000)
+                .recordStats()
+                .build();
+        CacheInfo.registerCache("Tags (dictionary)", tagsCache);
     }
-    
+
 //    public OperationRequest getOperationRequest(Request req)
 //    {
 //        return new OperationRequest(req);
@@ -42,11 +53,13 @@ public class OperationHelper
      */
     public String[][] getTags(String tableName, String valueColumnName, String textColumnName)
     {
-        List<String[]> tags = db.selectList("SELECT "+valueColumnName+", "+textColumnName+" FROM " + tableName,
-                rs -> new String[]{rs.getString(valueColumnName), rs.getString(textColumnName)}
-        );
-        String[][] stockArr = new String[tags.size()][2];
-        return tags.toArray(stockArr);
+        return tagsCache.get(tableName+ "getTags" + valueColumnName + "," + textColumnName + UserInfoHolder.getLanguage(), k -> {
+            List<String[]> tags = db.selectList("SELECT " + valueColumnName + ", " + textColumnName + " FROM " + tableName,
+                    rs -> new String[]{rs.getString(valueColumnName), rs.getString(textColumnName)}
+            );
+            String[][] stockArr = new String[tags.size()][2];
+            return tags.toArray(stockArr);
+        });
     }
 
 //    public List<Option> formOptionsWithEmptyValue(String tableName, String valueColumnName, String textColumnName, String placeholder)
@@ -57,7 +70,7 @@ public class OperationHelper
 //
 //        return options.build();
 //    }
-    
+
     /**
      * <p>
      * Creates a list of options by a specified table name. An entity with a
@@ -65,35 +78,36 @@ public class OperationHelper
      * of the entity must be defined too. Roles and visibility of the query are
      * ignored.
      * </p>
-     * 
+     *
      * <p>
      * A "selection view" query is a query with name "*** Selection view ***"
      * that selects rows with two fields: an identifier and a displayed text.
      * Names of columns are ignored, only the order matters.
      * </p>
-     * 
+     *
      * @throws IllegalArgumentException when an entity or a query is not defined
      * @throws Error if a found query cannot be compiled
      */
     public String[][] getTagsFromSelectionView(String tableName)
     {
-        Optional<Query> foundQuery = meta.findQuery(tableName, DatabaseConstants.SELECTION_VIEW);
+        return tagsCache.get(tableName + "getTagsFromSelectionView" + UserInfoHolder.getLanguage(), k -> {
+            Optional<Query> foundQuery = meta.findQuery(tableName, DatabaseConstants.SELECTION_VIEW);
 
-        if (!foundQuery.isPresent())
-            throw new IllegalArgumentException();
+            if (!foundQuery.isPresent())
+                throw new IllegalArgumentException();
 
-        try
-        {
-            List<String[]> tags = db.selectList(foundQuery.get().getQueryCompiled().validate(), rs ->
-                    new String[]{rs.getString(1), rs.getString(2)}
-            );
-            String[][] stockArr = new String[tags.size()][2];
-            return tags.toArray(stockArr);
-        }
-        catch (ProjectElementException e)
-        {
-            throw Be5Exception.internalInQuery(e, foundQuery.get());
-        }
+            try
+            {
+                List<String[]> tags = db.selectList(foundQuery.get().getQueryCompiled().validate(), rs ->
+                        new String[]{rs.getString(1), rs.getString(2)}
+                );
+                String[][] stockArr = new String[tags.size()][2];
+                return tags.toArray(stockArr);
+            } catch (ProjectElementException e)
+            {
+                throw Be5Exception.internalInQuery(e, foundQuery.get());
+            }
+        });
     }
 
     public String[][] getTagsFromEnum(String tableName, String name)
@@ -103,41 +117,49 @@ public class OperationHelper
 
     public String[][] getTagsFromEnum(String tableName, String operationName, String name)
     {
-        ColumnDef column = meta.getColumn(tableName, name);
+        return tagsCache.get(tableName + "getTagsFromEnum" + operationName + "," + name + UserInfoHolder.getLanguage(), k -> {
+            ColumnDef column = meta.getColumn(tableName, name);
 
-        if (column == null)throw new IllegalArgumentException();
-        String[] enumValues = column.getType().getEnumValues();
+            if (column == null) throw new IllegalArgumentException();
+            String[] enumValues = column.getType().getEnumValues();
 
-        String[][] stockArr = new String[enumValues.length][2];
+            String[][] stockArr = new String[enumValues.length][2];
 
-        for (int i = 0; i < enumValues.length; i++)
-        {
-            if(operationName != null){
-                stockArr[i] = new String[]{enumValues[i], userAwareMeta.getLocalizedOperationField(tableName, operationName, enumValues[i])};
-            }else{
-                stockArr[i] = new String[]{enumValues[i], userAwareMeta.getLocalizedOperationField(tableName, enumValues[i])};
+            for (int i = 0; i < enumValues.length; i++)
+            {
+                if (operationName != null)
+                {
+                    stockArr[i] = new String[]{enumValues[i], userAwareMeta.getLocalizedOperationField(tableName, operationName, enumValues[i])};
+                } else
+                {
+                    stockArr[i] = new String[]{enumValues[i], userAwareMeta.getLocalizedOperationField(tableName, enumValues[i])};
+                }
             }
-        }
 
-        return stockArr;
+            return stockArr;
+        });
     }
 
     public String[][] getTagsYesNo()
     {
-        String[][] arr = new String[2][2];
-        arr[0] = new String[]{yes, userAwareMeta.getColumnTitle("query.jsp", "page", yes)};
-        arr[1] = new String[]{no, userAwareMeta.getColumnTitle("query.jsp", "page", no)};
-        return arr;
+        return tagsCache.get("getTagsYesNo" + UserInfoHolder.getLanguage(), k -> {
+            String[][] arr = new String[2][2];
+            arr[0] = new String[]{yes, userAwareMeta.getColumnTitle("query.jsp", "page", yes)};
+            arr[1] = new String[]{no, userAwareMeta.getColumnTitle("query.jsp", "page", no)};
+            return arr;
+        });
     }
 
     public String[][] getTagsNoYes()
     {
-        String[][] arr = new String[2][2];
-        arr[0] = new String[]{no, userAwareMeta.getColumnTitle("query.jsp", "page", no)};
-        arr[1] = new String[]{yes, userAwareMeta.getColumnTitle("query.jsp", "page", yes)};
-        return arr;
+        return tagsCache.get("getTagsNoYes" + UserInfoHolder.getLanguage(), k -> {
+            String[][] arr = new String[2][2];
+            arr[0] = new String[]{no, userAwareMeta.getColumnTitle("query.jsp", "page", no)};
+            arr[1] = new String[]{yes, userAwareMeta.getColumnTitle("query.jsp", "page", yes)};
+            return arr;
+        });
     }
-
+//
 //    public List<Option> formOptionsWithEmptyValue(String tableName, String placeholder)
 //    {
 //        ImmutableList.Builder<Option> options = ImmutableList.builder();
@@ -146,5 +168,5 @@ public class OperationHelper
 //
 //        return options.build();
 //    }
-    
+
 }
