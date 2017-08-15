e377d03777b35ee8b572fc8a46f2ba53119a262a
uuinnk <uuinnk@developmentontheedge.com>
2017-08-15 13:48:12 +0700

    add SilentDeleteOperation

--- server/src/main/java/com/developmentontheedge/be5/operations/DeleteOperation.java
+++ server/src/main/java/com/developmentontheedge/be5/operations/DeleteOperation.java
@@ -2,25 +2,16 @@ package com.developmentontheedge.be5.operations;
 
 import com.developmentontheedge.be5.operation.Operation;
 import com.developmentontheedge.be5.operation.OperationContext;
-import com.developmentontheedge.be5.operation.OperationSupport;
-
-import java.util.Map;
+import com.developmentontheedge.be5.operation.OperationResult;
 
 
-public class DeleteOperation extends OperationSupport implements Operation
+public class DeleteOperation extends SilentDeleteOperation implements Operation
 {
-
-    @Override
-    public Object getParameters(Map<String, Object> presetValues) throws Exception
-    {
-        return null;
-    }
-
     @Override
     public void invoke(Object parameters, OperationContext context) throws Exception
     {
-        db.update(sqlHelper.generateDeleteInSql(getInfo().getEntity(), context.getRecordIDs().length),
-                sqlHelper.getDeleteValuesWithSpecial(getInfo().getEntity(), context.getRecordIDs()));
-    }
+        super.invoke(parameters, context);
 
+        setResult(OperationResult.finished());
+    }
 }
---
+++ server/src/main/java/com/developmentontheedge/be5/operations/SilentDeleteOperation.java
@@ -0,0 +1,23 @@
+package com.developmentontheedge.be5.operations;
+
+import com.developmentontheedge.be5.operation.Operation;
+import com.developmentontheedge.be5.operation.OperationContext;
+import com.developmentontheedge.be5.operation.OperationSupport;
+
+import java.util.Map;
+
+public class SilentDeleteOperation extends OperationSupport implements Operation
+{
+    @Override
+    public Object getParameters(Map<String, Object> presetValues) throws Exception
+    {
+        return null;
+    }
+
+    @Override
+    public void invoke(Object parameters, OperationContext context) throws Exception
+    {
+        db.update(sqlHelper.generateDeleteInSql(getInfo().getEntity(), context.getRecordIDs().length),
+                sqlHelper.getDeleteValuesWithSpecial(getInfo().getEntity(), context.getRecordIDs()));
+    }
+}
