941bb6436c7fde89d2add4d727d3a23ac72db69d
Innokentiy <uu.innk@gmail.com>
2017-08-19 23:29:00 +0700

    return Throwable getMessage

--- server/src/main/java/com/developmentontheedge/be5/operation/OperationResult.java
+++ server/src/main/java/com/developmentontheedge/be5/operation/OperationResult.java
@@ -123,8 +123,7 @@ public class OperationResult
 
     public static OperationResult error(Throwable details)
     {
-        //todo сериализуется Throwable в json - зачем?
-        return new OperationResult(OperationStatus.ERROR, details); 
+        return new OperationResult(OperationStatus.ERROR, details.getMessage());
     }
 
     @Override
