b9c29bdd0102ef4456f1d49db0ed5e12024dce33
QProgS <uu.innk@gmail.com>
2017-09-02 16:34:53 +0700

    add SessionVariablesEditTest

---
+++ modules/core/src/test/groovy/com/developmentontheedge/be5/modules/core/operations/SessionVariablesEditTest.groovy
@@ -0,0 +1,48 @@
+package com.developmentontheedge.be5.modules.core.operations
+
+import com.developmentontheedge.be5.api.Request
+import com.developmentontheedge.be5.model.FormPresentation
+import com.developmentontheedge.be5.test.SqlMockOperationTest
+import com.developmentontheedge.be5.test.mocks.SqlServiceMock
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+import static org.mockito.Matchers.anyString
+import static org.mockito.Matchers.anyVararg
+import static org.mockito.Mockito.verify
+import static org.mockito.Mockito.when
+
+class SessionVariablesEditTest extends SqlMockOperationTest
+{
+    @Test
+    void testGet()
+    {
+        Request req = getSpyMockRecForOp("_system_", "Session variables", "SessionVariablesEdit", "remoteAddr",
+                "", ["remoteAddr":"199.168.0.1"])
+
+        FormPresentation first = operationService.generate(req).getFirst()
+
+        assertEquals("{" +
+                "'values':{" +
+                    "'label':'Тип: java.lang.String'," +
+                    "'newValue':'199.168.0.1'}," +
+                "'meta':{" +
+                    "'/label':{'displayName':'label','labelField':true}," +
+                    "'/newValue':{'displayName':'Новое значение:'}}," +
+                "'order':['/label','/newValue']" +
+            "}", oneQuotes(first.getBean().toString()))
+    }
+
+    @Test
+    void testInvoke()
+    {
+        Request req = getSpyMockRecForOp("_system_", "Session variables", "SessionVariablesEdit", "remoteAddr",
+                "{'newValue':'199.168.0.2'}", ["remoteAddr":"199.168.0.1"])
+
+        when(SqlServiceMock.mock.getScalar(anyString(), anyVararg())).thenReturn(1L)
+
+        operationService.execute(req)
+
+        verify(req).setAttribute("remoteAddr", "199.168.0.2")
+    }
+}
