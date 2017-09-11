031d2cd1b87425136a7350e5f427f446ea99ac5b
QProgS <uu.innk@gmail.com>
2017-09-11 22:49:02 +0700

    update from generateEntity branch

--- server/src/main/java/com/developmentontheedge/be5/model/beans/DynamicPropertyGBuilder.java
+++ server/src/main/java/com/developmentontheedge/be5/model/beans/DynamicPropertyGBuilder.java
@@ -6,13 +6,14 @@ import com.developmentontheedge.beans.DynamicPropertySet;
 import groovy.lang.Closure;
 import groovy.lang.DelegatesTo;
 
+
 public class DynamicPropertyGBuilder
 {
-    public static DynamicProperty add(DynamicPropertySet dynamicPropertySet, @DelegatesTo(strategy = Closure.DELEGATE_ONLY,
+    public static DynamicProperty add(DynamicPropertySet dynamicPropertySet, @DelegatesTo(strategy = Closure.DELEGATE_FIRST,
             value = DPSAttributes.class) Closure cl){
         DPSAttributes builder = new DPSAttributes();
         Closure code = cl.rehydrate(builder, builder, builder);
-        code.setResolveStrategy(Closure.DELEGATE_ONLY);
+        code.setResolveStrategy(Closure.DELEGATE_FIRST);
         code.call();
 
         DynamicProperty dynamicProperty = new DynamicProperty(builder.getName(), builder.getTYPE());
@@ -20,4 +21,14 @@ public class DynamicPropertyGBuilder
         return DynamicPropertyMetaClass.leftShift(dynamicProperty, builder.getMap());
     }
 
+    public static DynamicProperty edit(DynamicPropertySet dynamicPropertySet, String propertyName,
+                                       @DelegatesTo(strategy = Closure.DELEGATE_FIRST, value = DPSAttributes.class) Closure cl){
+        DPSAttributes builder = new DPSAttributes();
+        Closure code = cl.rehydrate(builder, builder, builder);
+        code.setResolveStrategy(Closure.DELEGATE_FIRST);
+        code.call();
+
+        DynamicProperty dynamicProperty = dynamicPropertySet.getProperty(propertyName);
+        return DynamicPropertyMetaClass.leftShift(dynamicProperty, builder.getMap());
+    }
 }
