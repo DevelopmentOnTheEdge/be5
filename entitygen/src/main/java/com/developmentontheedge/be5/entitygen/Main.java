975e39a14ea421c8d18da3de90a17bf2345b0ed4
QProgS <uu.innk@gmail.com>
2017-09-16 20:24:43 +0700

    entity-gen stub

---
+++ entitygen/src/main/java/com/developmentontheedge/be5/entitygen/Main.java
@@ -0,0 +1,19 @@
+package com.developmentontheedge.be5.entitygen;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.util.Date;
+
+
+public class Main
+{
+
+    public static void main(String[] args) throws IOException
+    {
+
+    }
+}
