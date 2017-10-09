06308fda057d0702679cffc1bb899a87509116bf
Innokentiy <uu.innk@gmail.com>
2017-10-09 23:52:09 +0700

    add check null

--- modules/core/src/groovy/operations/system/SessionVariablesEdit.groovy
+++ modules/core/src/groovy/operations/system/SessionVariablesEdit.groovy
@@ -10,18 +10,26 @@ class SessionVariablesEdit extends OperationSupport implements Operation
     @Override
     Object getParameters(Map<String, Object> presetValues) throws Exception
     {
-        dps << [
-                name        : "label",
-                value       : "Тип: " + session[records[0]].getClass().getName(),
-                LABEL_FIELD : true
-        ]
+        def variable = session[records[0]]
+        if(variable != null)
+        {
+            dps << [
+                    name        : "label",
+                    value       : "Тип: " + variable.getClass().getName(),
+                    LABEL_FIELD : true
+            ]
 
-        dps << [
-                name        : "newValue",
-                TYPE        : session[records[0]].getClass(),
-                DISPLAY_NAME: "Новое значение:",
-                value       : presetValues.getOrDefault("newValue", session[records[0]])
-        ]
+            dps << [
+                    name        : "newValue",
+                    TYPE        : variable.getClass(),
+                    DISPLAY_NAME: "Новое значение:",
+                    value       : presetValues.getOrDefault("newValue", variable)
+            ]
+        }
+        else
+        {
+            dps = dpsHelper.getDpsWithLabelANDNotSubmitted("Session variable '${records[0]}' not found")
+        }
 
         return dps
     }
