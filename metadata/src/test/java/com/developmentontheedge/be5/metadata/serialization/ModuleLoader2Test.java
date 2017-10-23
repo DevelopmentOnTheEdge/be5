8242b0756c4828174451cf44036bd15a8076f39d
QProgS <uu.innk@gmail.com>
2017-10-24 01:31:09 +0700

    add setUp

--- metadata/src/test/java/com/developmentontheedge/be5/metadata/serialization/ModuleLoader2Test.java
+++ metadata/src/test/java/com/developmentontheedge/be5/metadata/serialization/ModuleLoader2Test.java
@@ -2,31 +2,18 @@ package com.developmentontheedge.be5.metadata.serialization;
 
 import com.developmentontheedge.be5.metadata.exception.ProjectLoadException;
 import com.developmentontheedge.be5.metadata.exception.ProjectSaveException;
-import com.developmentontheedge.be5.metadata.model.ColumnDef;
-import com.developmentontheedge.be5.metadata.model.DataElementUtils;
-import com.developmentontheedge.be5.metadata.model.Entity;
-import com.developmentontheedge.be5.metadata.model.EntityType;
-import com.developmentontheedge.be5.metadata.model.Operation;
-import com.developmentontheedge.be5.metadata.model.PageCustomization;
 import com.developmentontheedge.be5.metadata.model.Project;
-import com.developmentontheedge.be5.metadata.model.Query;
-import com.developmentontheedge.be5.metadata.model.SpecialRoleGroup;
-import com.developmentontheedge.be5.metadata.model.TableDef;
 import com.developmentontheedge.be5.metadata.sql.Rdbms;
-import one.util.streamex.StreamEx;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URL;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -37,14 +24,21 @@ public class ModuleLoader2Test
     @Rule
     public TemporaryFolder tmp = new TemporaryFolder();
 
+    private Path path;
+    private Project project;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        path = tmp.newFolder().toPath();
+        project = new Project( "test" );
+        project.setRoles( Arrays.asList( "Administrator", "Guest", "User", "Operator" ) );
+        project.setDatabaseSystem( Rdbms.POSTGRESQL );
+    }
+
     @Test
     public void loadAllProjectsTest() throws IOException, ProjectSaveException, ProjectLoadException
     {
-        Path path = tmp.newFolder().toPath();
-        Project project = new Project( "test" );
-        project.setRoles( Arrays.asList( "Administrator", "Guest", "User", "Operator" ) );
-        project.setDatabaseSystem( Rdbms.POSTGRESQL );
-
         Serialization.save( project, path );
 
         ModuleLoader2.loadAllProjects(Collections.singletonList(path.resolve("project.yaml").toUri().toURL()));
@@ -53,11 +47,6 @@ public class ModuleLoader2Test
     @Test
     public void loadAllProjectsTestWithDev() throws IOException, ProjectSaveException, ProjectLoadException
     {
-        Path path = tmp.newFolder().toPath();
-        Project project = new Project( "test" );
-        project.setRoles( Arrays.asList( "Administrator", "Guest", "User", "Operator" ) );
-        project.setDatabaseSystem( Rdbms.POSTGRESQL );
-
         Serialization.save( project, path );
 
         try( PrintWriter out = new PrintWriter( path.resolve("dev.yaml").toFile() ) )
