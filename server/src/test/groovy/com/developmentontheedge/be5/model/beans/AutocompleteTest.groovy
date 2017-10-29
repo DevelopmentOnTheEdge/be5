467841dda96919fbbc08f7a800e0ed558907403c
QProgS <uu.innk@gmail.com>
2017-10-30 02:28:22 +0700

    add test

--- server/src/test/groovy/com/developmentontheedge/be5/model/beans/AutocompleteTest.groovy
+++ server/src/test/groovy/com/developmentontheedge/be5/model/beans/AutocompleteTest.groovy
@@ -43,6 +43,7 @@ class AutocompleteTest extends Be5ProjectTest
             COLUMN_SIZE_ATTR = 10
             MESSAGE          = "Can't be null"
             STATUS           = Validation.Status.ERROR
+            CSS_CLASSES      = "col-lg-6"
         }
 
         add (dps, "input2") {
@@ -53,7 +54,7 @@ class AutocompleteTest extends Be5ProjectTest
             CAN_BE_NULL = true
         }
 
-        assertEquals("{'values':{'reasonMulti':123,'input2':'value2'},'meta':{'/reasonMulti':{'displayName':'Множественный выбор','type':'Integer','hidden':true,'reloadOnChange':true,'reloadOnFocusOut':true,'rawValue':true,'groupName':'Test','groupId':'1','readOnly':true,'multipleSelectionList':true,'passwordField':true,'labelField':true,'columnSize':'10','status':'error','message':'Can't be null','defaultValue':'1234','tagList':[['fired','Уволен'],['other','Иная причина']],'extraAttrs':[],'validationRules':{'attr':'digits','type':'baseRule'}},'/input2':{'displayName':'input2','canBeNull':true}},'order':['/reasonMulti','/input2']}",
+        assertEquals("{'values':{'reasonMulti':123,'input2':'value2'},'meta':{'/reasonMulti':{'displayName':'Множественный выбор','type':'Integer','hidden':true,'reloadOnChange':true,'reloadOnFocusOut':true,'rawValue':true,'groupName':'Test','groupId':'1','readOnly':true,'multipleSelectionList':true,'passwordField':true,'labelField':true,'cssClasses':'col-lg-6','columnSize':'10','status':'error','message':'Can't be null','defaultValue':'1234','tagList':[['fired','Уволен'],['other','Иная причина']],'extraAttrs':[],'validationRules':{'attr':'digits','type':'baseRule'}},'/input2':{'displayName':'input2','canBeNull':true}},'order':['/reasonMulti','/input2']}",
                 oneQuotes(JsonFactory.dps(dps).toString()))
     }
 
