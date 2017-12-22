d05d7178f62b35e32334d4df4bfbd8c7a065bd37
uuinnk <uuinnk@developmentontheedge.com>
2017-12-22 19:10:41 +0700

    removed unnecessary tests

--- sql/src/test/java/com/developmentontheedge/sql/FilterApplierTest.java
+++ sql/src/test/java/com/developmentontheedge/sql/FilterApplierTest.java
@@ -2,6 +2,8 @@ package com.developmentontheedge.sql;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -19,32 +21,31 @@ import com.developmentontheedge.sql.model.AstStart;
 import com.developmentontheedge.sql.model.DefaultParserContext;
 import com.developmentontheedge.sql.model.SqlQuery;
 
+
 public class FilterApplierTest
 {
     @Test
     public void testSetFilterApplier()
     {
-        AstStart query = SqlQuery.parse( "SELECT g.yr, city.country, g.city FROM games g, city WHERE g.city = city.name" );
-        Map<ColumnRef, Object> conditions = EntryStream.<String, Object>of( "city.country", "UK", "g.yr", 2012 )
+        AstStart query = SqlQuery.parse( "SELECT * FROM games g, city WHERE g.city = city.name" );
+        Map<ColumnRef, Object> conditions = EntryStream.<String, Object>of( "city.country", "UK", "games.yr", 2012 )
                 .mapKeys( key -> ColumnRef.resolve( query, key ) ).toCustomMap(LinkedHashMap::new);
 
         new FilterApplier().setFilter( query, conditions );
-        assertEquals( "SELECT g.yr, city.country, g.city FROM games g, city WHERE city.country ='UK' AND g.yr = 2012",
-                new Formatter().format( query, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
-        assertEquals( "SELECT g.yr, city.country, g.city FROM games g, city WHERE city.country ='UK' AND g.yr = 2012",
+
+        assertEquals( "SELECT * FROM games g, city WHERE city.country ='UK' AND g.yr = 2012",
                 new Formatter().format( query, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
 
         AstStart query2 = SqlQuery.parse( "SELECT city.name, g.* FROM city INNER JOIN games g ON (g.city = city.name)" );
         new FilterApplier().setFilter( query2, conditions );
-        assertEquals( "SELECT city.name, g.* FROM city INNER JOIN games g WHERE city.country ='UK' AND g.yr = 2012",
-                new Formatter().format( query2, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
+
         assertEquals( "SELECT city.name, g.* FROM city INNER JOIN games g WHERE city.country ='UK' AND g.yr = 2012",
                 new Formatter().format( query2, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
 
+
         AstStart query3 = SqlQuery.parse( "SELECT * FROM city JOIN games g ON (g.city = city.name) JOIN games gm ON city.country ='UK'" );
         new FilterApplier().setFilter( query3, conditions );
-        assertEquals( "SELECT * FROM city INNER JOIN games g INNER JOIN games gm WHERE city.country ='UK' AND g.yr = 2012",
-                new Formatter().format( query3, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
+
         assertEquals( "SELECT * FROM city INNER JOIN games g INNER JOIN games gm WHERE city.country ='UK' AND g.yr = 2012",
                 new Formatter().format( query3, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
     }
@@ -55,15 +56,12 @@ public class FilterApplierTest
         AstStart query = SqlQuery.parse( "SELECT name FROM bbc WHERE name LIKE 'Z%' UNION SELECT name FROM actor WHERE name LIKE 'Z%'" );
         Map<ColumnRef, Object> conditions = Collections.singletonMap( ColumnRef.resolve( query, "name" ), "name" );
         new FilterApplier().setFilter( query, conditions );
-        assertEquals( "SELECT * FROM (SELECT name FROM bbc UNION SELECT name FROM actor) tmp WHERE name ='name'",
-                new Formatter().format( query, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
+
         assertEquals( "SELECT * FROM (SELECT name FROM bbc UNION SELECT name FROM actor) tmp WHERE name ='name'",
                 new Formatter().format( query, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
 
 //        conditions = Collections.singletonMap( ColumnRef.resolve( query, "name1" ), "name1" );
 //        new FilterApplier().setFilter( query, conditions );
-//        assertEquals( "SELECT * FROM (SELECT name FROM bbc UNION SELECT name FROM actor) tmp WHERE name1 ='name1'",
-//                new Formatter().format( query, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
 //        assertEquals( "SELECT * FROM (SELECT name FROM bbc UNION SELECT name FROM actor) tmp WHERE name1 ='name1'",
 //                new Formatter().format( query, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
     }
@@ -72,46 +70,41 @@ public class FilterApplierTest
     public void testAddFilterApplier()
     {
         AstStart query = SqlQuery
-                .parse( "SELECT games.yr gy, city.country, games.city FROM games, city WHERE games.city = city.name AND city.country = 'UK'" );
+                .parse( "SELECT * FROM games, city WHERE games.city = city.name AND city.country = 'UK'" );
         Map<ColumnRef, Object> conditions = Collections.singletonMap( ColumnRef.resolve( query, "games.yr" ), 2012 );
         new FilterApplier().addFilter( query, conditions );
-        assertEquals( "SELECT games.yr gy, city.country, games.city FROM games, city WHERE games.city = city.name AND city.country = 'UK' AND games.yr = 2012",
-                new Formatter().format( query, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
-        assertEquals( "SELECT games.yr gy, city.country, games.city FROM games, city WHERE games.city = city.name AND city.country = 'UK' AND games.yr = 2012",
+        assertEquals( "SELECT * FROM games, city WHERE games.city = city.name AND city.country = 'UK' AND games.yr = 2012",
                 new Formatter().format( query, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
 
         query = SqlQuery.parse( "SELECT * FROM games RIGHT JOIN city ON (games.city = city.name) WHERE city.country ='UK'" );
         new FilterApplier().addFilter( query, conditions );
-        assertEquals( "SELECT * FROM games RIGHT JOIN city ON (games.city = city.name) WHERE city.country ='UK' AND games.yr = 2012",
-                new Formatter().format( query, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
+
         assertEquals( "SELECT * FROM games RIGHT JOIN city ON (games.city = city.name) WHERE city.country ='UK' AND games.yr = 2012",
                 new Formatter().format( query, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
 
         query = SqlQuery.parse( "SELECT * FROM games RIGHT JOIN city ON (games.city = city.name) WHERE city.country ='UK' OR city.active = 'yes'" );
         new FilterApplier().addFilter( query, conditions );
-        assertEquals( "SELECT * FROM games RIGHT JOIN city ON (games.city = city.name) WHERE ( city.country ='UK' OR city.active = 'yes') AND games.yr = 2012",
-                new Formatter().format( query, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
+
         assertEquals( "SELECT * FROM games RIGHT JOIN city ON (games.city = city.name) WHERE ( city.country ='UK' OR city.active = 'yes') AND games.yr = 2012",
                 new Formatter().format( query, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
 
-        query = SqlQuery
-                .parse( "SELECT games.yr gy, city.country, games.city FROM games, city ORDER BY 1" );
+        query = SqlQuery.parse( "SELECT * FROM games, city ORDER BY 1" );
         new FilterApplier().addFilter( query, conditions );
-        assertEquals( "SELECT games.yr gy, city.country, games.city FROM games, city WHERE games.yr = 2012 ORDER BY 1",
-                new Formatter().format( query, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
-        assertEquals( "SELECT games.yr gy, city.country, games.city FROM games, city WHERE games.yr = 2012 ORDER BY 1",
+
+        assertEquals( "SELECT * FROM games, city WHERE games.yr = 2012 ORDER BY 1",
                 new Formatter().format( query, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
     }
 
     @Test
-    public void testAddFilterApplierUnion()
+    public void testAddFilterApplierUnion() throws ParseException
     {
-        AstStart query = SqlQuery.parse( "SELECT name FROM bbc WHERE name LIKE 'Z%' UNION SELECT name FROM actor WHERE name LIKE 'Z%'" );
-        Map<ColumnRef, Object> conditions = Collections.singletonMap( ColumnRef.resolve( query, "name" ), "name" );
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+
+        AstStart query = SqlQuery.parse( "SELECT name FROM bbc b WHERE name LIKE 'Z%' UNION SELECT name FROM actor WHERE name LIKE 'Z%'" );
+        Map<ColumnRef, Object> conditions = Collections.singletonMap( ColumnRef.resolve( query, "bbc.data" ), new java.sql.Date(format.parse("22-12-2017").getTime()) );
         new FilterApplier().addFilter( query, conditions );
-        assertEquals( "SELECT * FROM (SELECT name FROM bbc WHERE name LIKE 'Z%' UNION SELECT name FROM actor WHERE name LIKE 'Z%') tmp WHERE name ='name'",
-                new Formatter().format( query, new Context( Dbms.MYSQL ), new DefaultParserContext() ) );
-        assertEquals( "SELECT * FROM (SELECT name FROM bbc WHERE name LIKE 'Z%' UNION SELECT name FROM actor WHERE name LIKE 'Z%') tmp WHERE name ='name'",
+
+        assertEquals( "SELECT * FROM (SELECT name FROM bbc b WHERE name LIKE 'Z%' UNION SELECT name FROM actor WHERE name LIKE 'Z%') tmp WHERE b.data ='2017-12-22'",
                 new Formatter().format( query, new Context( Dbms.POSTGRESQL ), new DefaultParserContext() ) );
     }
 }
