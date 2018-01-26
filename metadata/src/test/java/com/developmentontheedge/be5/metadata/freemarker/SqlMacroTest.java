259a26434f012c6ad596ce5f226af96bbf5e62c4
uuinnk <uuinnk@developmentontheedge.com>
2018-01-26 18:45:58 +0700

    postgres test

--- metadata/src/test/java/com/developmentontheedge/be5/metadata/freemarker/SqlMacroTest.java
+++ metadata/src/test/java/com/developmentontheedge/be5/metadata/freemarker/SqlMacroTest.java
@@ -17,35 +17,17 @@ public class SqlMacroTest
     public void testMacro() throws ProjectElementException
     {
         Project project = new Project("test");
-        project.setDatabaseSystem( Rdbms.SQLSERVER );
-        FreemarkerScript script = new FreemarkerScript("script", project.getApplication().getFreemarkerScripts());
-        DataElementUtils.save( script );
-        script.setSource( "SELECT ${concat('a'?asDate, 'b', 'c'?str)} FROM test" );
-        assertEquals("SELECT ( CONVERT( DATE, a, 120 ) + b + 'c' ) FROM test", project.mergeTemplate( script ).validate());
-        script.setSource( "<#macro _sql>${project.enterSQL()}<#assign nested><#nested></#assign>${project.translateSQL(nested)}</#macro>" +
-                "<@_sql>SELECT TO_DATE(a) || b || 'c' FROM test</@>" );
-        assertEquals("SELECT CONVERT(DATE, a, 120)+ b + 'c' FROM test", project.mergeTemplate( script ).validate());
-        script.setSource( "<#macro _sql>${project.enterSQL()}<#assign nested><#nested></#assign>${project.translateSQL(nested)}</#macro>" +
-                "<@_sql>SELECT ${'a'?asDate} || b || 'c' FROM test</@>" );
-        assertEquals("SELECT CONVERT(DATE, a, 120)+ b + 'c' FROM test", project.mergeTemplate( script ).validate());
-    }
-
-    @Test
-    public void testMacroPostgres() throws ProjectElementException
-    {
-        Project project = new Project("test");
         project.setDatabaseSystem( Rdbms.POSTGRESQL );
         FreemarkerScript script = new FreemarkerScript("script", project.getApplication().getFreemarkerScripts());
         DataElementUtils.save( script );
         script.setSource( "SELECT ${concat('a'?asDate, 'b', 'c'?str)} FROM test" );
         assertEquals("SELECT ( TO_DATE(a,'YYYY-MM-DD') || b || 'c' ) FROM test", project.mergeTemplate( script ).validate());
-
-        script.setSource( "SELECT ${'a'?asDate} || b || 'c' FROM test" );
-        assertEquals("SELECT TO_DATE(a,'YYYY-MM-DD') || b || 'c' FROM test", project.mergeTemplate( script ).validate());
-//todo
-//        script.setSource( "SELECT TO_DATE(a) || b || 'c' FROM test" );
-//        assertEquals("SELECT TO_DATE(a,'YYYY-MM-DD') || b || 'c' FROM test", project.mergeTemplate( script ).validate());
+        script.setSource( "<#macro _sql>${project.enterSQL()}<#assign nested><#nested></#assign>${project.translateSQL(nested)}</#macro>" +
+                "<@_sql>SELECT TO_DATE(a) || b || 'c' FROM test</@>" );
+        assertEquals("SELECT TO_DATE(a, 'YYYY-MM-DD') || b || 'c' FROM test", project.mergeTemplate( script ).validate());
+        script.setSource( "<#macro _sql>${project.enterSQL()}<#assign nested><#nested></#assign>${project.translateSQL(nested)}</#macro>" +
+                "<@_sql>SELECT ${'a'?asDate} || b || 'c' FROM test</@>" );
+        assertEquals("SELECT TO_DATE(a, 'YYYY-MM-DD') || b || 'c' FROM test", project.mergeTemplate( script ).validate());
     }
-
 
 }
