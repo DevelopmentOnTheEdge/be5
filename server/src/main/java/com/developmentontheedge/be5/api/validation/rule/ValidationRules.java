2819919f13d41b6bebba692fc007fad9aae2964d
uuinnk <uuinnk@developmentontheedge.com>
2018-03-15 19:35:56 +0700

    refactoring, add customMessage

--- server/src/main/java/com/developmentontheedge/be5/api/validation/rule/ValidationRules.java
+++ server/src/main/java/com/developmentontheedge/be5/api/validation/rule/ValidationRules.java
@@ -5,59 +5,105 @@ import com.developmentontheedge.beans.json.JsonFactory;
 
 public class ValidationRules
 {
-    private String type;
-    private Object attr;
-
-    private ValidationRules(String type, Object attr)
+    public static ValidationRule range(long min, long max)
     {
-        this.type = type;
-        this.attr = attr;
+        return new ValidationRule("range", new Range(min, max));
     }
 
-    public static ValidationRules range(long min, long max)
+    public static ValidationRule range(double min, double max)
     {
-        return new ValidationRules("range", new Range(min, max));
+        return new ValidationRule("range", new DoubleRange(min, max));
     }
 
-    public static ValidationRules range(double min, double max)
+    public static ValidationRule range(long min, long max, String customMessage)
     {
-        return new ValidationRules("range", new DoubleRange(min, max));
+        return new ValidationRule("range", new Range(min, max), customMessage);
     }
 
-    public static ValidationRules step(long value)
+    public static ValidationRule range(double min, double max, String customMessage)
     {
-        return new ValidationRules("step", value);
+        return new ValidationRule("range", new DoubleRange(min, max), customMessage);
     }
 
-    public static ValidationRules step(double value)
+    public static ValidationRule step(long value)
     {
-        return new ValidationRules("step", value);
+        return new ValidationRule("step", value);
     }
 
-    public static ValidationRules pattern(String value)
+    public static ValidationRule step(double value)
     {
-        return new ValidationRules("pattern", value);
+        return new ValidationRule("step", value);
     }
 
-    public static ValidationRules unique(String tableName)
+    public static ValidationRule step(long value, String customMessage)
     {
-        return new ValidationRules("unique", new Unique(tableName));
+        return new ValidationRule("step", value, customMessage);
     }
 
-    public String getType()
+    public static ValidationRule step(double value, String customMessage)
     {
-        return type;
+        return new ValidationRule("step", value, customMessage);
     }
 
-    public Object getAttr()
+    public static ValidationRule pattern(String value)
     {
-        return attr;
+        return new ValidationRule("pattern", value);
     }
 
-    @Override
-    public String toString()
+    public static ValidationRule pattern(String value, String customMessage)
     {
-        return JsonFactory.jsonb.toJson(this, this.getClass());
+        return new ValidationRule("pattern", value, customMessage);
+    }
+
+    public static ValidationRule unique(String tableName)
+    {
+        return new ValidationRule("unique", new Unique(tableName));
+    }
+
+    public static ValidationRule unique(String tableName, String customMessage)
+    {
+        return new ValidationRule("unique", new Unique(tableName), customMessage);
+    }
+
+    public static class ValidationRule
+    {
+        private String type;
+        private Object attr;
+        private String customMessage;
+
+        ValidationRule(String type, Object attr)
+        {
+            this.type = type;
+            this.attr = attr;
+        }
+
+        ValidationRule(String type, Object attr, String customMessage)
+        {
+            this.type = type;
+            this.attr = attr;
+            this.customMessage = customMessage;
+        }
+
+        public String getType()
+        {
+            return type;
+        }
+
+        public Object getAttr()
+        {
+            return attr;
+        }
+
+        public String getCustomMessage()
+        {
+            return customMessage;
+        }
+
+        @Override
+        public String toString()
+        {
+            return JsonFactory.jsonb.toJson(this, this.getClass());
+        }
     }
 
     public static class Range
@@ -99,13 +145,25 @@ public class ValidationRules
     public static class Unique
     {
         String tableName;
+        String columnName;
 
         Unique(String tableName) {
             this.tableName = tableName;
         }
 
+        Unique(String tableName, String columnName)
+        {
+            this.tableName = tableName;
+            this.columnName = columnName;
+        }
+
         public String getTableName() {
             return tableName;
+        }
+
+        public String getColumnName()
+        {
+            return columnName;
         }
     }
 }
