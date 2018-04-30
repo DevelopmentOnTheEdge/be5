f5297145c45d374552a087e35f1aa3cc25b05434
Innokentiy <uu.innk@gmail.com>
2018-04-30 19:18:05 +0700

    remove old experimental

--- entitygen/src/main/java/com/developmentontheedge/be5/entitygen/experimental/genegate/entities/Provinces.java
+++
@@ -1,9 +0,0 @@
-package com.developmentontheedge.be5.entitygen.experimental.genegate.entities;
-
-
-public class Provinces
-{
-    public String ID;
-    public String name;
-    public String countryID;
-}
--- entitygen/src/main/java/com/developmentontheedge/be5/entitygen/experimental/genegate/entities/SystemSettings.java
+++
@@ -1,8 +0,0 @@
-package com.developmentontheedge.be5.entitygen.experimental.genegate.entities;
-
-public class SystemSettings
-{
-    public String setting_name;
-    public String section_name;
-    public String setting_value;
-}
