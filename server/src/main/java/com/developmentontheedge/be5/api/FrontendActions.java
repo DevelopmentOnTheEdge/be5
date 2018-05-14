e0395ddec2f649c2cfec6b4acc0d81abc9ca46e9
uuinnk <uuinnk@developmentontheedge.com>
2018-05-14 19:32:40 +0700

    add CLOSE_MAIN_MODAL, UPDATE_DOCUMENT actions, change logic mainModalClose

--- server/src/main/java/com/developmentontheedge/be5/api/FrontendActions.java
+++ server/src/main/java/com/developmentontheedge/be5/api/FrontendActions.java
@@ -6,18 +6,30 @@ import com.developmentontheedge.be5.model.jsonapi.JsonApiModel;
 
 public interface FrontendActions
 {
+    String CLOSE_MAIN_MODAL = "CLOSE_MAIN_MODAL";
+
+    String UPDATE_DOCUMENT = "UPDATE_DOCUMENT";
     String UPDATE_PARENT_DOCUMENT = "UPDATE_PARENT_DOCUMENT";
     String GO_BACK = "GO_BACK";
     String OPEN_NEW_WINDOW = "OPEN_NEW_WINDOW";
     String REDIRECT = "REDIRECT";
 
-    //String UPDATE_DOCUMENT = "UPDATE_DOCUMENT";
-
+    FrontendAction CLOSE_MAIN_MODAL_ACTION = new FrontendAction(CLOSE_MAIN_MODAL, null);
     FrontendAction GO_BACK_ACTION = new FrontendAction(GO_BACK, null);
+
+    static FrontendAction updateDocument(JsonApiModel model)
+    {
+        return new FrontendAction(UPDATE_DOCUMENT, model);
+    }
 
     static FrontendAction updateParentDocument(JsonApiModel model)
     {
         return new FrontendAction(UPDATE_PARENT_DOCUMENT, model);
+    }
+
+    static FrontendAction closeMainModal()
+    {
+        return CLOSE_MAIN_MODAL_ACTION;
     }
 
     static FrontendAction goBack()
