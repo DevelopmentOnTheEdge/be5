db74afbba121d5321f7d4cabb40373a6c8950e2d
Innokentiy Krivonosov <uu.innk@gmail.com>
2018-06-02 19:03:00 +0700

    fix

--- base/src/test/java/com/developmentontheedge/be5/base/util/DpsUtilsTest.java
+++ base/src/test/java/com/developmentontheedge/be5/base/util/DpsUtilsTest.java
@@ -1,4 +1,4 @@
-package com.developmentontheedge.be5.util;
+package com.developmentontheedge.be5.base.util;
 
 import com.developmentontheedge.beans.DynamicPropertyBuilder;
 import com.developmentontheedge.beans.DynamicPropertySetSupport;
