bad5f7e8e06b19f979b06f00996c94a05aeb2930
uuinnk <uuinnk@developmentontheedge.com>
2018-07-05 15:42:08 +0700

    add QueryUtilsFilterTest tests

--- query/src/test/java/com/developmentontheedge/be5/query/impl/utils/QueryUtilsFilterTest.java
+++ query/src/test/java/com/developmentontheedge/be5/query/impl/utils/QueryUtilsFilterTest.java
@@ -3,7 +3,6 @@ package com.developmentontheedge.be5.query.impl.utils;
 import com.developmentontheedge.be5.query.QueryBe5ProjectDBTest;
 import com.developmentontheedge.sql.model.AstStart;
 import com.developmentontheedge.sql.model.SqlQuery;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -41,5 +40,29 @@ public class QueryUtilsFilterTest extends QueryBe5ProjectDBTest
 
         assertEquals("SELECT ft.name, ft.value\n" +
                 "FROM filterTestTable ft WHERE ft.name ='test'", ast.format());
+    }
+
+    @Test
+    public void ignoreKeywords() throws Exception
+    {
+        AstStart ast = SqlQuery.parse(meta.getQuery("filterTestTable", "Simple").getQueryCompiled().validate().trim());
+        QueryUtils.applyFilters(ast, "filterTestTable", Collections.singletonMap("_search_", Collections.singletonList("test")));
+
+        assertEquals("SELECT ft.name, ft.value\n" +
+                "FROM filterTestTable ft", ast.format());
+    }
+
+    @Test
+    public void ignoreUsedParams() throws Exception
+    {
+        AstStart ast = SqlQuery.parse(meta.getQuery("filterTestTable", "With Parameter").getQueryCompiled().validate().trim());
+        QueryUtils.applyFilters(ast, "filterTestTable", Collections.singletonMap("name", Collections.singletonList("test")));
+
+        assertEquals("SELECT ft.name, ft.value\n" +
+                "FROM filterTestTable ft\n" +
+                "WHERE (1 = 1)\n" +
+                "<if parameter=\"name\">\n" +
+                "  AND ft.name LIKE <parameter:name />\n" +
+                "</if>", ast.format());
     }
 }
