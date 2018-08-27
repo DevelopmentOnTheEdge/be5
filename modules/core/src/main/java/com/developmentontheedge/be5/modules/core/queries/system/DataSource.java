52cc06af4fd84703ed0fc4834d79b9793de46be1
uuinnk <uuinnk@developmentontheedge.com>
2018-08-27 18:29:28 +0700

    DataSource query - fix for many MBeanInfo, refactoring

--- modules/core/src/main/java/com/developmentontheedge/be5/modules/core/queries/system/DataSource.java
+++ modules/core/src/main/java/com/developmentontheedge/be5/modules/core/queries/system/DataSource.java
@@ -1,11 +1,10 @@
 package com.developmentontheedge.be5.modules.core.queries.system;
 
 import com.developmentontheedge.be5.database.DataSourceService;
+import com.developmentontheedge.be5.database.DbService;
 import com.developmentontheedge.be5.query.model.TableModel;
 import com.developmentontheedge.be5.server.queries.support.TableBuilderSupport;
 import org.apache.commons.dbcp.BasicDataSource;
-import org.codehaus.groovy.runtime.DefaultGroovyMethods;
-import org.codehaus.groovy.runtime.StringGroovyMethods;
 
 import javax.inject.Inject;
 import javax.management.MBeanAttributeInfo;
@@ -20,79 +19,77 @@ import java.util.TreeMap;
 
 public class DataSource extends TableBuilderSupport
 {
-    @Inject
-    private DataSourceService databaseService;
+    @Inject private DataSourceService databaseService;
+    @Inject private DbService db;
 
     @Override
     public TableModel getTableModel()
     {
         addColumns("name", "value");
-
         if (databaseService.getDataSource() instanceof BasicDataSource)
         {
-            Map<String, String> parameters = getParameters((BasicDataSource) databaseService.getDataSource());
-            for (Map.Entry<String, String> entry : parameters.entrySet())
-            {
-                addRow(cells(entry.getKey(), entry.getValue() != null ? entry.getValue() : ""));
-            }
-
+            getBasicDataSourceInfo();
         }
         else
         {
-            try
-            {
-                MBeanServer server = ManagementFactory.getPlatformMBeanServer();
-                Set<ObjectName> objectNames = server.queryNames(null, null);
-                for (ObjectName name : objectNames)
-                {
-                    MBeanInfo info = server.getMBeanInfo(name);
-                    DefaultGroovyMethods.println(this, info.getClassName());
-                    if (info.getClassName().equals("org.apache.tomcat.jdbc.pool.jmx.ConnectionPool"))
-                    {
-
-                        for (MBeanAttributeInfo mf : info.getAttributes())
-                        {
-                            Object attributeValue = server.getAttribute(name, mf.getName());
-                            if (attributeValue != null)
-                            {
-                                addRow(cells(mf.getName(), attributeValue.toString()));
-                            }
-
-                        }
-
-                        break;
-                    }
-
-                }
-
-            }
-            catch (Exception e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
+            getMBeanInfo();
         }
-
-
         return table(columns, rows);
     }
 
-    public static Map<String, String> getParameters(BasicDataSource dataSource)
+    private void getMBeanInfo()
     {
-        Map<String, String> map = new TreeMap<>();
+        String connectUrl = db.transactionWithResult(conn -> conn.getMetaData().getURL());
+        try
+        {
+            MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+            Set<ObjectName> objectNames = server.queryNames(null, null);
+            for (ObjectName name : objectNames)
+            {
+                MBeanInfo info = server.getMBeanInfo(name);
+                if (info.getClassName().equals("org.apache.tomcat.jdbc.pool.jmx.ConnectionPool")
+                        && connectUrl.equals(server.getAttribute(name, "Url")))
+                {
+                    for (MBeanAttributeInfo mf : info.getAttributes())
+                    {
+                        Object attributeValue = server.getAttribute(name, mf.getName());
+                        if (attributeValue != null)
+                        {
+                            addRow(cells(mf.getName(), attributeValue.toString()));
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 
-        map.put("DataSource class", dataSource.getClass().getCanonicalName());
-        map.put("Active/Idle", StringGroovyMethods.plus(dataSource.getNumActive(), " / ") + dataSource.getNumIdle());
-        map.put("max Active/max Idle", StringGroovyMethods.plus(dataSource.getMaxActive(), " / ") + dataSource.getMaxIdle());
-        map.put("max wait", StringGroovyMethods.plus(dataSource.getMaxWait(), ""));
-        map.put("Username", dataSource.getUsername());
-        map.put("DefaultCatalog", dataSource.getDefaultCatalog());
-        map.put("DriverClassName", dataSource.getDriverClassName());
-        map.put("Url", dataSource.getUrl());
-        map.put("ValidationQuery", dataSource.getValidationQuery());
-        map.put("ConnectionInitSqls", dataSource.getConnectionInitSqls().toString());
+    private void getBasicDataSourceInfo()
+    {
+        Map<String, String> parameters = getParameters((BasicDataSource) databaseService.getDataSource());
+        for (Map.Entry<String, String> entry : parameters.entrySet())
+        {
+            addRow(cells(entry.getKey(), entry.getValue() != null ? entry.getValue() : ""));
+        }
+    }
 
-        return map;
+    private Map<String, String> getParameters(BasicDataSource dataSource)
+    {
+        return new TreeMap<String, String>() {{
+            put("DataSource class", dataSource.getClass().getCanonicalName());
+            put("Active/Idle", dataSource.getNumActive() + " / " + dataSource.getNumIdle());
+            put("max Active/max Idle", dataSource.getMaxActive() + " / " + dataSource.getMaxIdle());
+            put("max wait", dataSource.getMaxWait() + "");
+            put("Username", dataSource.getUsername());
+            put("DefaultCatalog", dataSource.getDefaultCatalog());
+            put("DriverClassName", dataSource.getDriverClassName());
+            put("Url", dataSource.getUrl());
+            put("ValidationQuery", dataSource.getValidationQuery());
+            put("ConnectionInitSqls", dataSource.getConnectionInitSqls().toString());
+        }};
     }
 }
