b8b83f9453f19bd250d6a0c43a9715e733c4be07
uuinnk <uuinnk@developmentontheedge.com>
2018-09-04 17:52:29 +0700

    log mail body

--- test-base/src/main/java/com/developmentontheedge/be5/test/mocks/TestMailService.java
+++ test-base/src/main/java/com/developmentontheedge/be5/test/mocks/TestMailService.java
@@ -4,12 +4,14 @@ import com.developmentontheedge.be5.base.services.MailService;
 
 import javax.mail.internet.InternetAddress;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import static org.mockito.Mockito.mock;
 
 
 public class TestMailService implements MailService
 {
+    public static final Logger log = Logger.getLogger(TestMailService.class.getName());
     public static MailService mock = mock(MailService.class);
 
     public static void newMock()
@@ -21,23 +23,27 @@ public class TestMailService implements MailService
     public void sendPlainEmail(String to, String subject, String body)
     {
         mock.sendPlainEmail(to, subject, body);
+        log.info(body);
     }
 
     @Override
     public void sendHtmlEmail(String to, String subject, String body)
     {
         mock.sendHtmlEmail(to, subject, body);
+        log.info(body);
     }
 
     @Override
     public void sendEmail(InternetAddress from, InternetAddress[] to, String subject, String body, String type) throws Exception
     {
         mock.sendEmail(from, to, subject, body, type);
+        log.info(body);
     }
 
     @Override
     public void sendEmailReal(InternetAddress from, InternetAddress[] to, String subject, String body, String type, Map locMessages) throws Exception
     {
         mock.sendEmailReal(from, to, subject, body, type, locMessages);
+        log.info(body);
     }
 }
