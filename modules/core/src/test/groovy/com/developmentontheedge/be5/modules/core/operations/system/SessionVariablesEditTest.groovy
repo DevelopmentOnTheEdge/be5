272589b1fa42a80f8100102550fb5e437ae47ffb
uuinnk <uuinnk@developmentontheedge.com>
2018-09-18 15:02:43 +0700

    fix tests

--- modules/core/src/test/groovy/com/developmentontheedge/be5/modules/core/operations/system/SessionVariablesEditTest.groovy
+++ modules/core/src/test/groovy/com/developmentontheedge/be5/modules/core/operations/system/SessionVariablesEditTest.groovy
@@ -1,21 +1,16 @@
 package com.developmentontheedge.be5.modules.core.operations.system
 
 import com.developmentontheedge.be5.metadata.RoleType
-import com.developmentontheedge.be5.modules.core.CoreBe5ProjectDBTest
+import com.developmentontheedge.be5.modules.core.CoreBe5ProjectDbMockTest
 import com.developmentontheedge.be5.operation.model.OperationStatus
-import com.developmentontheedge.be5.test.BaseTestUtils
-import com.developmentontheedge.be5.test.mocks.DbServiceMock
 import com.developmentontheedge.beans.json.JsonFactory
 import org.junit.Before
 import org.junit.Test
 
 import static org.junit.Assert.assertEquals
-import static org.mockito.Matchers.anyString
-import static org.mockito.Matchers.anyVararg
-import static org.mockito.Mockito.when
 
 
-class SessionVariablesEditTest extends CoreBe5ProjectDBTest
+class SessionVariablesEditTest extends CoreBe5ProjectDbMockTest
 {
     @Before
     void setUp()
@@ -38,16 +33,13 @@ class SessionVariablesEditTest extends CoreBe5ProjectDBTest
                 "'/label':{'displayName':'label','labelField':true}," +
                 "'/newValue':{'displayName':'Новое значение:'}}," +
                 "'order':['/label','/newValue']" +
-                "}", BaseTestUtils.oneQuotes(JsonFactory.bean(first)))
+                "}", oneQuotes(JsonFactory.bean(first)))
     }
 
     @Test
     void testInvoke()
     {
         session.set("remoteAddr", "199.168.0.1")
-        //setSession("remoteAddr", "199.168.0.1")
-
-        when(DbServiceMock.mock.one(anyString(), anyVararg())).thenReturn(1L)
 
         def operation = executeOperation("_system_", "Session variables", "SessionVariablesEdit", "remoteAddr", ["newValue": "199.168.0.2"])
         assertEquals(OperationStatus.REDIRECTED, operation.getSecond().getStatus())
