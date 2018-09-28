197a69499ae2ed251e54c0b116ca328952829471
Innokentiy <uu.innk@gmail.com>
2018-09-28 13:46:49 +0700

    add ClearAllCaches instead ReloadProject #107

--- modules/core/src/main/java/com/developmentontheedge/be5/modules/core/operations/system/ClearAllCaches.java
+++ modules/core/src/main/java/com/developmentontheedge/be5/modules/core/operations/system/ClearAllCaches.java
@@ -1,19 +1,19 @@
 package com.developmentontheedge.be5.modules.core.operations.system;
 
-import com.developmentontheedge.be5.base.services.ProjectProvider;
+import com.developmentontheedge.be5.base.services.Be5Caches;
 import com.developmentontheedge.be5.server.operations.support.OperationSupport;
 
 import javax.inject.Inject;
 
 
-public class ReloadProject extends OperationSupport
+public class ClearAllCaches extends OperationSupport
 {
     @Inject
-    private ProjectProvider projectProvider;
+    private Be5Caches be5Caches;
 
     @Override
     public void invoke(Object parameters) throws Exception
     {
-        projectProvider.reloadProject();
+        be5Caches.clearAll();
     }
 }
