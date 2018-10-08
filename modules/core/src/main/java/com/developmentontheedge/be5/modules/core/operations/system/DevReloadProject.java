344eec637ce92ef816dd4082ac8a6fe72b78747e
uuinnk <uuinnk@developmentontheedge.com>
2018-10-08 16:40:28 +0700

    add DevReloadProject operation

---
+++ modules/core/src/main/java/com/developmentontheedge/be5/modules/core/operations/system/DevReloadProject.java
@@ -0,0 +1,31 @@
+package com.developmentontheedge.be5.modules.core.operations.system;
+
+import com.developmentontheedge.be5.base.services.ProjectProvider;
+import com.developmentontheedge.be5.operation.model.OperationResult;
+import com.developmentontheedge.be5.server.operations.support.OperationSupport;
+import com.google.inject.Stage;
+
+import javax.inject.Inject;
+
+
+public class DevReloadProject extends OperationSupport
+{
+    @Inject
+    private ProjectProvider projectProvider;
+
+    @Inject
+    private Stage stage;
+
+    @Override
+    public void invoke(Object parameters) throws Exception
+    {
+        if (stage == Stage.DEVELOPMENT)
+        {
+            projectProvider.reloadProject();
+        }
+        else
+        {
+            setResult(OperationResult.error("Only in DEVELOPMENT stage"));
+        }
+    }
+}
