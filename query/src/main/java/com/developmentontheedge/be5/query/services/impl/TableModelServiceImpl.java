3da52731248f29e7d5525e21be5fb5538941b49f
uuinnk <uuinnk@developmentontheedge.com>
2018-10-12 12:26:41 +0700

    save QUERY_POSITIONS in session

--- query/src/main/java/com/developmentontheedge/be5/query/services/impl/TableModelServiceImpl.java
+++ query/src/main/java/com/developmentontheedge/be5/query/services/impl/TableModelServiceImpl.java
@@ -7,6 +7,7 @@ import com.developmentontheedge.be5.base.services.UserAwareMeta;
 import com.developmentontheedge.be5.base.services.UserInfoProvider;
 import com.developmentontheedge.be5.base.util.LayoutUtils;
 import com.developmentontheedge.be5.metadata.model.Query;
+import com.developmentontheedge.be5.query.QuerySession;
 import com.developmentontheedge.be5.query.TableBuilder;
 import com.developmentontheedge.be5.query.impl.SqlTableBuilder;
 import com.developmentontheedge.be5.query.model.TableModel;
@@ -15,6 +16,8 @@ import com.developmentontheedge.be5.query.services.TableModelService;
 import com.google.inject.Injector;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.developmentontheedge.be5.query.TableConstants.LIMIT;
@@ -25,16 +28,19 @@ import static com.developmentontheedge.be5.query.TableConstants.ORDER_DIR;
 
 public class TableModelServiceImpl implements TableModelService
 {
+    private static final String QUERY_POSITIONS = "QUERY_POSITIONS";
     private final UserAwareMeta userAwareMeta;
     private final CoreUtils coreUtils;
     private final GroovyRegister groovyRegister;
     private final Injector injector;
     private final QueryService queryService;
     private final UserInfoProvider userInfoProvider;
+    private final Provider<QuerySession> session;
 
     @Inject
     public TableModelServiceImpl(UserAwareMeta userAwareMeta, CoreUtils coreUtils, GroovyRegister groovyRegister,
-                                 Injector injector, QueryService queryService, UserInfoProvider userInfoProvider)
+                                 Injector injector, QueryService queryService, UserInfoProvider userInfoProvider,
+                                 Provider<QuerySession> session)
     {
         this.userAwareMeta = userAwareMeta;
         this.coreUtils = coreUtils;
@@ -42,6 +48,7 @@ public class TableModelServiceImpl implements TableModelService
         this.injector = injector;
         this.queryService = queryService;
         this.userInfoProvider = userInfoProvider;
+        this.session = session;
     }
 
     @Override
@@ -77,10 +84,11 @@ public class TableModelServiceImpl implements TableModelService
 
     private TableModel getSqlTableModel(Query query, Map<String, Object> parameters)
     {
-        int orderColumn = Integer.parseInt((String) parameters.getOrDefault(ORDER_COLUMN, "-1"));
-        String orderDir = (String) parameters.getOrDefault(ORDER_DIR, "asc");
-        int offset = Integer.parseInt((String) parameters.getOrDefault(OFFSET, "0"));
-        int limit = Integer.parseInt((String) parameters.getOrDefault(LIMIT, Integer.toString(Integer.MAX_VALUE)));
+        Map<String, String> positions = getUserQueryPositions(query);
+        int orderColumn = Integer.parseInt(getPosition(parameters, positions, ORDER_COLUMN, "-1"));
+        String orderDir = getPosition(parameters, positions, ORDER_DIR, "asc");
+        int offset = Integer.parseInt(getPosition(parameters, positions, OFFSET, "0"));
+        int limit = Integer.parseInt(getPosition(parameters, positions, LIMIT, Integer.toString(Integer.MAX_VALUE)));
 
         int maxLimit = userAwareMeta.getQuerySettings(query).getMaxRecordsPerPage();
 
@@ -96,6 +104,32 @@ public class TableModelServiceImpl implements TableModelService
                 .offset(offset)
                 .limit(Math.min(limit, maxLimit))
                 .build();
+    }
+
+    private String getPosition(Map<String, Object> parameters, Map<String, String> positions, String name, String defaultValue)
+    {
+        if (parameters.containsKey(name))
+        {
+            String value = (String) parameters.get(name);
+            positions.put(name, value);
+            return value;
+        }
+        else
+        {
+            return positions.getOrDefault(name, defaultValue);
+        }
+    }
+
+    private Map<String, String> getUserQueryPositions(Query query)
+    {
+        Map<String, Map<String, String>> positions = (Map<String, Map<String, String>>) session.get().get(QUERY_POSITIONS);
+        if (positions == null)
+        {
+            positions = new HashMap<>();
+            session.get().set(QUERY_POSITIONS, positions);
+        }
+        String queryKey = query.getEntity().getName() + "." + query.getName();
+        return positions.computeIfAbsent(queryKey, k -> new HashMap<>());
     }
 
     private TableModel getFromTableBuilder(Query query, Map<String, Object> parameters)
