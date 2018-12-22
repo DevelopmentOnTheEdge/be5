42700e157c31295305615b3159c58031a8325f5a
Innokentiy <uu.innk@gmail.com>
2018-12-22 23:08:58 +0700

    add more logging

--- util/jetty/src/main/java/com/developmentontheedge/be5/jetty/EmbeddedJettyUtils.java
+++ util/jetty/src/main/java/com/developmentontheedge/be5/jetty/EmbeddedJettyUtils.java
@@ -1,17 +1,70 @@
 package com.developmentontheedge.be5.jetty;
 
+import com.developmentontheedge.be5.base.services.impl.LogConfigurator;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class EmbeddedJettyUtils
 {
+    private static final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+    private Server jetty;
+    private String name = "Be5Jetty";
+    private int post = 8200;
+
     public static void runWebApp() throws Exception
     {
-        Server server = new Server(8200);
-        server.setHandler(getWebAppContext());
-        server.start();
-        server.join();
+        new EmbeddedJettyUtils().run();
+
+    }
+
+    public final void run()
+    {
+        Thread.currentThread().setName(name);
+
+        LogConfigurator.configure();
+
+        try
+        {
+            jetty = new Server(8200);
+            jetty.setHandler(getWebAppContext());
+            doStart();
+            logStarted();
+        }
+        catch (Exception e)
+        {
+            log.log(Level.SEVERE, "Unable to start " + name, e);
+            System.exit(1);
+        }
+
+        try
+        {
+            jetty.join();
+        }
+        catch (Exception e)
+        {
+            log.severe("Interrupted (most likely JVM is shutting down and this is safe to ignore)");
+        }
+    }
+
+    private void doStart() throws Exception
+    {
+        String version = this.jetty.getClass().getPackage().getImplementationVersion();
+        log.info("Trying to start jetty v" + version);
+        this.jetty.start();
+        log.info("Started jetty v" + version);
+    }
+
+    private void logStarted()
+    {
+        log.info("-------------------------------------------------------");
+        log.info("Be5 application running at");
+        log.info(" => http://localhost:" + post);
+        log.info("-------------------------------------------------------");
     }
 
     private static WebAppContext getWebAppContext()
