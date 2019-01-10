ab58519644fe98f068cd0d5eb6725ecb44ee0be1
uuinnk <uuinnk@developmentontheedge.com>
2019-01-10 12:54:08 +0700

    ControllerSupport - add catch errors, logs

--- web/src/main/java/com/developmentontheedge/be5/web/impl/ControllerSupport.java
+++ web/src/main/java/com/developmentontheedge/be5/web/impl/ControllerSupport.java
@@ -7,10 +7,15 @@ import com.developmentontheedge.be5.web.Response;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public abstract class ControllerSupport extends HttpServlet implements Controller
 {
+    private static final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     {
@@ -23,13 +28,37 @@ public abstract class ControllerSupport extends HttpServlet implements Controlle
         respond(request, response);
     }
 
-
     private void respond(HttpServletRequest request, HttpServletResponse response)
     {
         Request req = new RequestImpl(request);
         ResponseImpl res = new ResponseImpl(response);
-        generate(req, res);
+        try
+        {
+            generate(req, res);
+        }
+        catch (IllegalArgumentException e)
+        {
+            log.log(Level.SEVERE, "Error in controller", e);
+            res.sendAsJson(e.getMessage(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        catch (Throwable e)
+        {
+            log.log(Level.SEVERE, "Error in controller", e);
+            res.sendAsJson("Error in controller", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public abstract void generate(Request req, Response res);
+
+    protected void sendHtmlError(Response response, Exception e)
+    {
+        sendHtmlError(response, e, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    protected void sendHtmlError(Response response, Exception e, int status)
+    {
+        response.setStatus(status);
+        response.sendHtml(e.getMessage());
+        log.log(Level.SEVERE, "", e);
+    }
 }
