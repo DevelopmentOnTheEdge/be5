e01a1e3f122cf40554f0dafaec15b8e40e937918
Innokentiy <uu.innk@gmail.com>
2019-01-12 21:23:41 +0700

    ScopedServerModule extends AbstractModule instead ServletModule

--- server/src/main/java/com/developmentontheedge/be5/server/ScopedServerModule.java
+++ server/src/main/java/com/developmentontheedge/be5/server/ScopedServerModule.java
@@ -4,14 +4,14 @@ import com.developmentontheedge.be5.web.Request;
 import com.developmentontheedge.be5.web.Response;
 import com.developmentontheedge.be5.web.impl.RequestImpl;
 import com.developmentontheedge.be5.web.impl.ResponseImpl;
-import com.google.inject.servlet.ServletModule;
+import com.google.inject.AbstractModule;
 import com.google.inject.servlet.ServletScopes;
 
 
-public class ScopedServerModule extends ServletModule
+public class ScopedServerModule extends AbstractModule
 {
     @Override
-    protected void configureServlets()
+    protected void configure()
     {
         bind(Request.class).to(RequestImpl.class).in(ServletScopes.REQUEST);
         bind(Response.class).to(ResponseImpl.class).in(ServletScopes.REQUEST);
