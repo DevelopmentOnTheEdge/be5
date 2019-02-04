1ba86cb99e7469f40a958368a00b10176b312d17
uuinnk <uuinnk@developmentontheedge.com>
2019-02-04 12:51:04 +0700

    add QRecAsMapParser

---
+++ query/src/main/java/com/developmentontheedge/be5/query/sql/QRecAsMapParser.java
@@ -0,0 +1,17 @@
+package com.developmentontheedge.be5.query.sql;
+
+import com.developmentontheedge.be5.database.sql.ResultSetParser;
+import com.developmentontheedge.be5.query.model.beans.QRec;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class QRecAsMapParser implements ResultSetParser<Map<String, Object>>
+{
+    @Override
+    public Map<String, Object> parse(ResultSet rs) throws SQLException
+    {
+        return DpsRecordAdapter.addDp(new QRec(), rs).asMap();
+    }
+}
