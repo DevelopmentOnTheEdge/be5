6cec7b98cd0749b8de848ac3a3b43519756935d0
dimka <dimka@developmentontheedge.com>
2020-04-15 17:46:30 +0700

    мелочь: несколько методов для камунды

--- modules/bpmn/src/main/java/com/developmentontheedge/be5/modules/bpmn/BpmnService.java
+++ modules/bpmn/src/main/java/com/developmentontheedge/be5/modules/bpmn/BpmnService.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
+import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.camunda.bpm.engine.task.Task;
 
 public interface BpmnService
@@ -32,12 +33,44 @@ public interface BpmnService
      *
      * @return key for started process instance.
      */
-    public String startProcess(String key, Map<String, Object> variables);
+    public String startProcess(String processDefinitionKey, Map<String, Object> variables);
 
     /**
-     * @return list of current active tasks.
+     * Starts BPMN model with the specified id and variables.
+     *
+     * @return key for started process instance.
+     */
+    public String startProcess(String processDefinitionKey, String businessKey, Map<String, Object> variables);
+
+    /**
+     * Starts BPMN model with the specified id and variables.
+     *
+     * @return key for started process instance.
+     */
+    public ProcessDefinition getProcess(String key);
+
+    /**
+     * @return list of all processes current active tasks.
      */
     public List<Task> getActiveTasks();
+
+    /**
+     * @param processDefinitionKey
+     * @return list of tasks for process
+     */
+    public List<Task> getTasks(String processDefinitionKey);
+
+    /**
+     * @param businessKey
+     * @return list of tasks for process
+     */
+    public List<Task> getTasksByBusinessKey(String businessKey);
+
+    /**
+     * @param taskId
+     * @return Task
+     */
+    public Task getTask(String taskId);
 
     /**
      * Completes the specified task.
@@ -51,4 +84,11 @@ public interface BpmnService
 
     /** Set up variable value for specified execution. */
     public void setVariable(String executionId, String variableName, Object value);
+
+    /** Set up variable value for specified execution. */
+    public void setVariableByBusinessKey(String businessKey, String variableName, Object value);
+
+    /** Create signal event for notification. */
+    public void createSignalEvent(String signalName);
+
 }
