7897ab5b4dc7370d830ce17d64fbce243496d5bd
Sergey Zhatchenko <zha@DevelopmentOnTheEdge.com>
2020-04-18 15:56:37 +0700

    possbly right structure

--- base/src/main/java/com/developmentontheedge/be5/groovy/meta/DynamicPropertySetMetaClass.java
+++ base/src/main/java/com/developmentontheedge/be5/groovy/meta/DynamicPropertySetMetaClass.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import groovy.lang.MissingPropertyException;
 
 //public class DynamicPropertySetMetaClass<T extends DynamicPropertySet> extends GDynamicPropertySetMetaClass
 public class DynamicPropertySetMetaClass<T extends DynamicPropertySet> extends ExtensionMethodsMetaClass
