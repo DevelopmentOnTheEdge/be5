175cb0548d156d4969242da4d84bfe14b3b57f12
mixa <mixa@developmentontheedge.com>
2020-04-27 11:41:20 +0700

    test innerDPS

--- base/src/test/groovy/com/developmentontheedge/be5/model/GroovyDPSTest.groovy
+++ base/src/test/groovy/com/developmentontheedge/be5/model/GroovyDPSTest.groovy
@@ -1,6 +1,7 @@
 package com.developmentontheedge.be5.model
 
 import com.developmentontheedge.be5.groovy.GDynamicPropertySetSupport
+import com.developmentontheedge.beans.DynamicPropertySet
 import com.developmentontheedge.beans.json.JsonFactory
 import groovy.transform.TypeChecked
 import org.junit.Test
@@ -66,9 +67,13 @@ class GroovyDPSTest
 
         dps.edit("input3") { value = getNewValue() }
 
+        DynamicPropertySet innerDPS = new GDynamicPropertySetSupport()
+        innerDPS.add { name = "innerInput1"; value = "Inner input 1" }
+        dps.add("parent1", "Parent 1") { value = innerDPS };
+
         assertEquals("newValue", dps["input3"])
 
-        assertEquals("{'values':{'reasonMulti':123,'input2':'value2','input3':'newValue','input4':'input4value'},'meta':{'/reasonMulti':{'displayName':'Множественный выбор','type':'Integer','hidden':true,'reloadOnChange':true,'rawValue':true,'groupId':'1','groupName':'Test','groupClasses':'col-md-8','readOnly':true,'multipleSelectionList':true,'passwordField':true,'labelField':true,'cssClasses':'col-lg-6','columnSize':'10','inputSize':'10','placeholder':'Select...','status':'error','message':'Can't be null','defaultValue':'1234','tagList':[['fired','Уволен'],['other','Иная причина']],'extraAttrs':[],'validationRules':{'max':'10','min':'1'}},'/input2':{'displayName':'New Display Name','canBeNull':true},'/input3':{'displayName':'Input 3'},'/input4':{'displayName':'Input 4'}},'order':['/reasonMulti','/input2','/input3','/input4']}",
+        assertEquals("{'values':{'reasonMulti':123,'input2':'value2','input3':'newValue','input4':'input4value','parent1':{'innerInput1':'Inner input 1'}},'meta':{'/reasonMulti':{'displayName':'Множественный выбор','type':'Integer','hidden':true,'reloadOnChange':true,'rawValue':true,'groupId':'1','groupName':'Test','groupClasses':'col-md-8','readOnly':true,'multipleSelectionList':true,'passwordField':true,'labelField':true,'cssClasses':'col-lg-6','columnSize':'10','inputSize':'10','placeholder':'Select...','status':'error','message':'Can't be null','defaultValue':'1234','tagList':[['fired','Уволен'],['other','Иная причина']],'extraAttrs':[],'validationRules':{'max':'10','min':'1'}},'/input2':{'displayName':'New Display Name','canBeNull':true},'/input3':{'displayName':'Input 3'},'/input4':{'displayName':'Input 4'},'/parent1':{'displayName':'Parent 1','type':'GDynamicPropertySetSupport'},'/parent1/innerInput1':{'displayName':'innerInput1'}},'order':['/reasonMulti','/input2','/input3','/input4','/parent1','/parent1/innerInput1']}",
                 oneQuotes(JsonFactory.dps(dps).toString()))
     }
 
