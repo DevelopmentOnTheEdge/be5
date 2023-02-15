090ed971b06403445d196bedcdcfcc7be656a734
Sergey Zhatchenko <zha@DevelopmentOnTheEdge.com>
2023-02-15 17:44:38 +0700

    experiments

--- server/src/main/java/com/developmentontheedge/be5/server/services/DpsHelper.java
+++ server/src/main/java/com/developmentontheedge/be5/server/services/DpsHelper.java
@@ -420,8 +420,12 @@ public class DpsHelper
         }
         else if (modelElements.getClass() == JavaOperation.class || modelElements.getClass() == GroovyOperation.class)
         {
+            System.err.println( "colName = " + colName );  
+            System.err.println( "columnDef.getEntity().getName() = " + columnDef.getEntity().getName() );  
+            System.err.println( "modelElements.getName() = " + modelElements.getName() );  
             String displayName = userAwareMeta.getLocalizedOperationField(
                     columnDef.getEntity().getName(), modelElements.getName(), colName );
+            System.err.println( "displayName = " + displayName );  
             if( displayName != null && !displayName.equals( colName ) )
             {
                 dp.setDisplayName( displayName );
