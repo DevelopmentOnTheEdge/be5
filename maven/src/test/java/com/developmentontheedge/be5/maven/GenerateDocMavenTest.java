5be523bbf6f8d6fdeea83c86b8c0006151d46387
unknown <fkolpakov@gmail.com>
2023-10-29 20:16:05 +0300

    test project for documentation

--- maven/src/test/java/com/developmentontheedge/be5/maven/GenerateDocMavenTest.java
+++ maven/src/test/java/com/developmentontheedge/be5/maven/GenerateDocMavenTest.java
@@ -1,27 +1,18 @@
 package com.developmentontheedge.be5.maven;
 
-import com.developmentontheedge.be5.metadata.util.NullLogger;
-
 import java.io.File;
-
-import org.junit.Before;
 import org.junit.Test;
 
-
-public class GenerateDocMavenTest extends TestMavenUtils
+public class GenerateDocMavenTest
 {
-    @Test
+	@Test
     public void validate()
     {
     	GenerateDocMojo mojo = new GenerateDocMojo();
 
-        mojo.projectPath = tpmProjectPath.toFile();
-    	mojo.docPath = new File("target/test-classes/doc");
-    	//mojo.logger = new NullLogger();
-    	
-    	//mojo.projectPath = new File("C:/projects/sirius/sirius-erp");
-    	//mojo.docPath     = new File("C:/projects/sirius/sirius-erp-docs_/source");
-      
-        mojo.execute();
+        mojo.projectPath = new File("target/test-classes/project");
+    	mojo.docPath     = new File("target/test-classes/doc/source");
+
+    	mojo.execute();
     }
 }
